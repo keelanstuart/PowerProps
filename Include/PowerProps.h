61cb223ad182250b939adc74057921744bb08dcd
Keelan Stuart <keelanstuart@gmail.com>
2021-05-17 14:50:01 -0400

    Static build support

--- Include/PowerProps.h
+++ Include/PowerProps.h
@@ -1,35 +1,39 @@
 /*
-PowerProps Library Source File
 
-Copyright © 2009-2021, Keelan Stuart. All rights reserved.
+	PowerProps Library Source File
 
-PowerProps is a generic property library which one can use to maintain
-easily discoverable data in a number of types, as well as convert that
-data to other formats and de/serialize in multiple modes
+	Copyright © 2009-2021, Keelan Stuart. All rights reserved.
 
-PowerProps is free software; you can redistribute it and/or modify it under
-the terms of the MIT License:
+	PowerProps is a generic property library which one can use to maintain
+	easily discoverable data in a number of types, as well as convert that
+	data to other formats and de/serialize in multiple modes
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+	PowerProps is free software; you can redistribute it and/or modify it under
+	the terms of the MIT License:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
 */
 
 #pragma once
+
+// If you use the static library version of GenIO, you must define GENIO_STATIC in your project's preprocessor definitions
 
 #if !defined(POWERPROPS_STATIC)
 
@@ -44,6 +48,7 @@ SOFTWARE.
 #define POWERPROPS_API
 
 #endif
+
 
 namespace props
 {
